898d70779e949c002f1476cb05acfd35463e17b9
Ashish Rawat <144007425+Ashish252003@users.noreply.github.com>
2025-04-10 12:23:45 +0530

    Add files via upload
    just a random mini project to learn new microsoft technology suvh as MudBlazor and Entitity framework

---
+++ Data/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeCrudBlazor.Data
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public  required string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+
+
+        public string ?PostalCode { get; set; }
+        public required string  Address { get; set; }
+        public  required string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? City { get; set; }
+        public  required string Designation { get; set; }
+
+    }
+}
---
+++ Data/EmployeeService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore; // Ensure this is added
+using EmployeeCrudBlazor.Context;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeCrudBlazor.Data
+{
+    public class EmployeeService
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public EmployeeService(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        // Get all employees
+        public async Task<List<Employee>> GetAllEmployees()
+        {
+            return await _applicationDbContext.Employees.ToListAsync();
+        }
+
+        // Add a new employee
+        public async Task<bool> AddNewEmployee(Employee employee)
+        {
+            await _applicationDbContext.Employees.AddAsync(employee);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+
+        // Get an employee by ID
+        public async Task<Employee> GetEmployeeById(int id)
+        {
+            Employee employee = await _applicationDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+            return employee;
+        }
+
+        //update Employee Data
+        public async Task<bool> UpdateEmployeeDetails(Employee employee)
+        {
+            _applicationDbContext.Employees.Update(employee);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+        
+        //remove the employee
+        public async Task<bool> DeleteEmployeee(Employee employee)
+        {
+            _applicationDbContext.Employees.Remove(employee);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+    }
+}
+
